02cb32eab7fdb3cb64ca7b952cc107bdb40dec89
Pavel Anokhov <p.anokhov@gmail.com>
2018-01-08 18:59:43 +0300

    AccountViewController: using default TableViewCells.

--- Adamant-ios/Stories/Account/AccountViewController.swift
+++ Adamant-ios/Stories/Account/AccountViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AccountViewController: UIViewController {
 	// MARK: - Constants
+	private let cellIdentifier = "cell"
+	
 	private enum Rows: Int {
 		case accountNumber = 0, balance, sendTokens
 	}
@@ -17,14 +19,10 @@ class AccountViewController: UIViewController {
 	
 	// MARK: - Dependencies
 	var loginService: LoginService!
-	var cellFactory: CellFactory!
 	
 	// MARK: - IBOutlets
 	@IBOutlet weak var tableView: UITableView!
 	
-	
-	// MARK: - Properties
-	var roundAvatarCellIdentifier: String!
 	
 	// MARK: - Lifecycle
 	
@@ -33,9 +31,7 @@ class AccountViewController: UIViewController {
 
 		tableView.delegate = self
 		tableView.dataSource = self
-		
-		roundAvatarCellIdentifier = SharedCell.RoundAvatar.cellIdentifier
-		tableView.register(cellFactory.nib(for: .RoundAvatar), forCellReuseIdentifier: roundAvatarCellIdentifier)
+		tableView.separatorInset = UIEdgeInsets.init(top: 0, left: 0, bottom: 0, right: 0)
 		
 		NotificationCenter.default.addObserver(forName: .userHasLoggedIn, object: nil, queue: nil) { _ in
 			self.tableView.reloadData()
@@ -53,6 +49,9 @@ class AccountViewController: UIViewController {
 
 // MARK: - UITableViewDataSource
 extension AccountViewController: UITableViewDataSource {
+	
+	// MARK: Configuring TableView
+	
 	func numberOfSections(in tableView: UITableView) -> Int {
 		return 1
 	}
@@ -69,31 +68,58 @@ extension AccountViewController: UITableViewDataSource {
 		return SharedCell.RoundAvatar.defaultRowHeight
 	}
 	
+	func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+		return UIView()
+	}
+	
+	
+	// MARK: Cells
+	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		guard let account = loginService.loggedAccount,
-			let row = Rows(rawValue: indexPath.row),
-			let cell = tableView.dequeueReusableCell(withIdentifier: roundAvatarCellIdentifier, for: indexPath) as? RoundAvatarTableViewCell else {
+			let row = Rows(rawValue: indexPath.row) else {
 			return UITableViewCell(style: .default, reuseIdentifier: nil)
+		}
+		
+		let cell: UITableViewCell
+		if let c = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+			cell = c
+		} else {
+			cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+			cell.accessoryType = .disclosureIndicator
+			cell.textLabel?.font = UIFont(name: "Exo 2", size: 17)
+			cell.detailTextLabel?.font = UIFont(name: "Exo 2", size: 12)
+			
+			if let main = UIColor(named: "Gray_main"),
+				let details = UIColor(named: "Gray_secondary") {
+				cell.textLabel?.textColor = main
+				cell.detailTextLabel?.textColor = details
+			}
 		}
 		
 		switch row {
 		case .accountNumber:
-			cell.mainText = "Your address"
-			cell.detailsText = account.address
+			cell.textLabel?.text = "Your address"
+			cell.detailTextLabel?.text = account.address
+			cell.imageView?.image = #imageLiteral(resourceName: "account")
 			
 		case .balance:
-			cell.mainText = "Your balance"
-			cell.detailsText = AdamantFormatters.format(balance: account.balance)
-			cell.accessoryType = .disclosureIndicator
+			cell.textLabel?.text = "Your balance"
+			cell.detailTextLabel?.text = AdamantFormatters.format(balance: account.balance)
+			cell.imageView?.image = #imageLiteral(resourceName: "wallet")
+			break
 			
 		case .sendTokens:
-			cell.mainText = "Send tokens"
-			cell.detailsText = nil
-			cell.accessoryType = .disclosureIndicator
+			cell.textLabel?.text = "Send tokens"
+			cell.detailTextLabel?.text = nil
+			cell.imageView?.image = #imageLiteral(resourceName: "send")
+			break
 		}
 		
 		return cell
 	}
+	
+	
 }
 
 
@@ -107,7 +133,7 @@ extension AccountViewController: UITableViewDelegate {
 		switch row {
 		case .accountNumber:
 			tableView.deselectRow(at: indexPath, animated: true)
-			// TODO: copy to buffer
+			// TODO: Open share menu
 			// TODO: show notification
 			
 		case .balance:
