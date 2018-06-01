3a619c0382faf11256b5a602edf47afaccf8ff3a
Pavel Anokhov <p.anokhov@gmail.com>
2018-06-01 16:57:05 +0300

    Transactions list and details scenes fixes ru localization file encoding fixed

--- Adamant/Stories/Transactions/TransactionDetailsViewController.swift
+++ Adamant/Stories/Transactions/TransactionDetailsViewController.swift
@@ -11,6 +11,12 @@ import SafariServices
 
 
 // MARK: - Localization
+extension String.adamantLocalized {
+	struct transactionDetails {
+		static let title = NSLocalizedString("TransactionDetailsScene.Title", comment: "Transaction details: scene title")
+	}
+}
+
 extension String.adamantLocalized.alert {
 	static let exportUrlButton = NSLocalizedString("TransactionDetailsScene.Share.URL", comment: "Export transaction: 'Share transaction URL' button")
 	static let exportSummaryButton = NSLocalizedString("TransactionDetailsScene.Share.Summary", comment: "Export transaction: 'Share transaction summary' button")
@@ -59,6 +65,7 @@ class TransactionDetailsViewController: UIViewController {
 	@IBOutlet weak var tableView: UITableView!
 	
 	override func viewDidLoad() {
+		navigationItem.title = String.adamantLocalized.transactionDetails.title
 		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(share))
 		tableView.dataSource = self
 		tableView.delegate = self
