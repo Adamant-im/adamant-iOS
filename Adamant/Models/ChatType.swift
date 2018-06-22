eed58b1581eaa6e21477b3fd7a7b4335c7c07d73
Pavel Anokhov <p.anokhov@gmail.com>
2018-06-22 19:22:01 +0300

    ChatType: extended, crashproof

--- Adamant/Models/ChatType.swift
+++ Adamant/Models/ChatType.swift
@@ -10,7 +10,46 @@ import Foundation
 
 /// - messageExpensive: Old message type, with 0.005 transaction fee
 /// - message: new and main message type, with 0.001 transaction fee
-enum ChatType: Int16, Codable {
-	case messageOld = 0
-	case message = 1
+/// - richMessage: json with additional data
+/// - signal: hidden system message for/from services
+enum ChatType: Codable {
+	case unknown(raw: Int16)
+	case messageOld // 0
+	case message // 1
+	case richMessage // 2
+	case signal // 3
+	
+	init(from int16: Int16) {
+		switch int16 {
+		case 0: self = .messageOld
+		case 1: self = .message
+		case 2: self = .richMessage
+		case 3: self = .signal
+			
+		default: self = .unknown(raw: int16)
+		}
+	}
+	
+	var rawValue: Int16 {
+		switch self {
+		case .messageOld: return 0
+		case .message: return 1
+		case .richMessage: return 2
+		case .signal: return 3
+			
+		case .unknown(let raw): return raw
+		}
+	}
+	
+	init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		let type = try container.decode(Int16.self)
+		
+		self.init(from: type)
+	}
+	
+	func encode(to encoder: Encoder) throws {
+		var container = encoder.singleValueContainer()
+		try container.encode(rawValue)
+	}
 }
