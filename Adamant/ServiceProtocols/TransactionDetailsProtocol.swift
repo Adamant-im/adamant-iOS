24ba6283acc9e21c44453e498a87f786c400adfc
Anton B <anton@absoftware.mobi>
2018-06-26 19:50:21 +0300

    Show Transaction details from chat
    Show Transaction details from chat
    Load Transaction details from Ethereum network

---
+++ Adamant/ServiceProtocols/TransactionDetailsProtocol.swift
@@ -0,0 +1,103 @@
+//
+//  TransactionDetailsProtocol.swift
+//  Adamant
+//
+//  Created by Anton Boyarkin on 26/06/2018.
+//  Copyright © 2018 Adamant. All rights reserved.
+//
+
+import Foundation
+import web3swift
+import BigInt
+
+/// A standard protocol representing a Transaction details.
+protocol TransactionDetailsProtocol {
+    
+    /// The identifier of the transaction.
+    var id: String { get }
+    
+    /// The sender of the transaction.
+    var senderAddress: String { get }
+    
+    /// The reciver of the transaction.
+    var recipientAddress: String { get }
+    
+    /// The date the transaction was sent.
+    var sentDate: Date { get }
+    
+    /// The amount of currency that was sent.
+    var amountValue: Double { get }
+    
+    /// The amount of fee that taken for transaction process.
+    var feeValue: Double { get }
+    
+    /// The confirmations of the transaction.
+    var confirmationsValue: String { get }
+    
+    /// The block of the transaction.
+    var block: String { get }
+    
+    /// The show explorer button.
+    var showGoToExplorer: Bool { get }
+    
+    /// The explorer url.
+    var explorerUrl: URL? { get }
+    
+    /// The show go to button.
+    var showGoToChat: Bool { get }
+    
+    /// The show go to button.
+    var chatroom: Chatroom? { get }
+    
+    /// The currency of the transaction.
+    var currencyCode: String { get }
+    
+}
+
+extension TransactionDetailsProtocol {
+    
+    var haveChatroom: Bool {
+        if let chatroom = self.chatroom, let transactions = chatroom.transactions  {
+            let messeges = transactions.first (where: { (object) -> Bool in
+                return !(object is TransferTransaction)
+            })
+            
+            return (messeges != nil)
+        }
+        
+        return false
+    }
+    
+    var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.roundingMode = .floor
+        formatter.positiveFormat = "#.######## \(currencyCode)"
+        
+        return formatter
+    }
+    
+    func getSummary() -> String {
+        return """
+        Transaction #\(id)
+        
+        Summary
+        Sender: \(senderAddress)
+        Recipient: \(recipientAddress)
+        Date: \(DateFormatter.localizedString(from: sentDate, dateStyle: .short, timeStyle: .medium))
+        Amount: \(formattedAmount())
+        Fee: \(formattedFee())
+        Confirmations: \(String(confirmationsValue))
+        Block: \(block)
+        URL: \(explorerUrl?.absoluteString ?? "")
+        """
+    }
+    
+    func formattedAmount() -> String {
+        return currencyFormatter.string(from: NSNumber(value: amountValue)) ?? ""
+    }
+    
+    func formattedFee() -> String {
+        return currencyFormatter.string(from: NSNumber(value: feeValue)) ?? ""
+    }
+}
--- Adamant/Services/EthApiService/EthApiService.swift
+++ Adamant/Services/EthApiService/EthApiService.swift
@@ -253,6 +253,42 @@ class EthApiService: EthApiServiceProtocol {
         }
     }
     
+    func getTransaction(byHash hash: String, completion: @escaping (ApiServiceResult<web3EthTransaction>) -> Void) {
+        DispatchQueue.global().async {
+            let result = self.web3.eth.getTransactionDetails(hash)
+            switch result {
+            case .success(let transaction):
+                if let number = transaction.blockNumber {
+//                    let resultReceipt = self.web3.eth.getTransactionReceipt(hash)
+//                    if case .success(let receipt) = resultReceipt {
+//                        print(receipt)
+//                    }
+                    
+                    let result = self.web3.eth.getBlockByNumber(number)
+                    guard case .success(let block) = result else {
+                        DispatchQueue.main.async {
+                            completion(.success(web3EthTransaction(transaction: transaction.transaction, transactionBlock: nil)))
+                        }
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        completion(.success(web3EthTransaction(transaction: transaction.transaction, transactionBlock: block)))
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(.success(web3EthTransaction(transaction: transaction.transaction, transactionBlock: nil)))
+                    }
+                }
+                break
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    completion(.failure(.internalError(message: "ETH Wallet: fail to load transaction details", error: error)))
+                }
+                break
+            }
+        }
+    }
+    
     // MARK: - Tools
     
     func getBalance(_ completion: @escaping (ApiServiceResult<String>) -> Void) {
@@ -396,268 +432,3 @@ class EthApiService: EthApiServiceProtocol {
         }
     }
 }
-
-
-/// A standard protocol representing a Transaction details.
-protocol TransactionDetailsProtocol {
-    
-    /// The identifier of the transaction.
-    var id: String { get }
-    
-    /// The sender of the transaction.
-    var senderAddress: String { get }
-    
-    /// The reciver of the transaction.
-    var recipientAddress: String { get }
-    
-    /// The date the transaction was sent.
-    var sentDate: Date { get }
-    
-    /// The amount of currency that was sent.
-    var amountValue: Double { get }
-    
-    /// The amount of fee that taken for transaction process.
-    var feeValue: Double { get }
-    
-    /// The confirmations of the transaction.
-    var confirmationsValue: String { get }
-    
-    /// The block of the transaction.
-    var block: String { get }
-    
-    /// The show explorer button.
-    var showGoToExplorer: Bool { get }
-    
-    /// The explorer url.
-    var explorerUrl: URL? { get }
-    
-    /// The show go to button.
-    var showGoToChat: Bool { get }
-    
-    /// The show go to button.
-    var chatroom: Chatroom? { get }
-    
-    /// The currency of the transaction.
-    var currencyCode: String { get }
-    
-}
-
-extension TransactionDetailsProtocol {
-    
-    var haveChatroom: Bool {
-        if let chatroom = self.chatroom, let transactions = chatroom.transactions  {
-            let messeges = transactions.first (where: { (object) -> Bool in
-                return !(object is TransferTransaction)
-            })
-            
-            return (messeges != nil)
-        }
-        
-        return false
-    }
-    
-    var currencyFormatter: NumberFormatter {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.roundingMode = .floor
-        formatter.positiveFormat = "#.######## \(currencyCode)"
-        
-        return formatter
-    }
-    
-    func getSummary() -> String {
-        return """
-        Transaction #\(id)
-        
-        Summary
-        Sender: \(senderAddress)
-        Recipient: \(recipientAddress)
-        Date: \(DateFormatter.localizedString(from: sentDate, dateStyle: .short, timeStyle: .medium))
-        Amount: \(formattedAmount())
-        Fee: \(formattedFee())
-        Confirmations: \(String(confirmationsValue))
-        Block: \(block)
-        URL: \(explorerUrl?.absoluteString ?? "")
-        """
-    }
-    
-    func formattedAmount() -> String {
-        return currencyFormatter.string(from: NSNumber(value: amountValue)) ?? ""
-    }
-    
-    func formattedFee() -> String {
-        return currencyFormatter.string(from: NSNumber(value: feeValue)) ?? ""
-    }
-}
-
-//MARK: - Decodable Transaction structures
-struct Response: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case status
-        case message
-        case result
-    }
-    
-    let status: Int
-    let message: String
-    let result: [EthTransaction]
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let statusString = (try? container.decode(String.self, forKey: .status)) ?? "0"
-        status = Int(statusString) ?? 0
-        message = (try? container.decode(String.self, forKey: .message)) ?? ""
-        result = (try? container.decode([EthTransaction].self, forKey: .result)) ?? []
-    }
-}
-
-struct EthTransaction: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case date = "timeStamp"
-        case hash
-        case value
-        case from
-        case to
-        case gasUsed
-        case gasPrice
-        case confirmations
-        case isError
-        case receiptStatus = "txreceipt_status"
-        case blockNumber
-    }
-    
-    let date: Date
-    let hash: String
-    let value: BigUInt
-    let from: String
-    let to: String
-    let gasUsed: BigUInt
-    let gasPrice: BigUInt
-    let confirmationsValue: String
-    let isError: Bool
-    let receiptStatus: TransactionReceiptStatus
-    let blockNumber: UInt
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        let unixTimeStamp = Double((try? container.decode(String.self, forKey: .date)) ?? "0") ?? 0
-        date = Date(timeIntervalSince1970: unixTimeStamp)
-        hash = (try? container.decode(String.self, forKey: .hash)) ?? ""
-        value = BigUInt((try? container.decode(String.self, forKey: .value)) ?? "0") ?? BigUInt(0)
-        from = (try? container.decode(String.self, forKey: .from)) ?? ""
-        to = (try? container.decode(String.self, forKey: .to)) ?? ""
-        gasUsed = BigUInt((try? container.decode(String.self, forKey: .gasUsed)) ?? "0") ?? BigUInt(0)
-        gasPrice = BigUInt((try? container.decode(String.self, forKey: .gasPrice)) ?? "0") ?? BigUInt(0)
-        confirmationsValue = (try? container.decode(String.self, forKey: .confirmations)) ?? "0"
-        let isErrorStatus = Int((try? container.decode(String.self, forKey: .isError)) ?? "0") ?? 0
-        isError = isErrorStatus == 1 ? true : false
-        receiptStatus = (try? container.decode(TransactionReceiptStatus.self, forKey: .receiptStatus)) ?? .unknown
-        blockNumber = UInt((try? container.decode(String.self, forKey: .blockNumber)) ?? "0") ?? 0
-    }
-    
-    func isOutgoing(_ address: String) -> Bool {
-        return from == address.lowercased() ? true : false
-    }
-    
-    func formattedValue() -> String {
-        if let formattedAmount = Web3.Utils.formatToEthereumUnits(value,
-                                                                  toUnits: .eth,
-                                                                  decimals: 8,
-                                                                  fallbackToScientific: true), let amount = Double(formattedAmount)  {
-            return "\(amount) ETH"
-        } else {
-            return "\(value)"
-        }
-    }
-}
-
-extension EthTransaction: TransactionDetailsProtocol {
-    
-    var id: String {
-        return self.hash
-    }
-    
-    var senderAddress: String {
-        return self.from
-    }
-    
-    var recipientAddress: String {
-        return self.to
-    }
-    
-    var sentDate: Date {
-        return self.date
-    }
-    
-    var amountValue: Double {
-        guard let string = Web3.Utils.formatToEthereumUnits(value, toUnits: .eth, decimals: 8), let value = Double(string) else {
-            return 0
-        }
-        
-        return value
-    }
-    
-    var feeValue: Double {
-        guard let string = Web3.Utils.formatToEthereumUnits((self.gasPrice * self.gasUsed), toUnits: .eth, decimals: 8), let value = Double(string) else {
-            return 0
-        }
-        
-        return value
-    }
-    
-    var block: String {
-        return "\(self.blockNumber)"
-    }
-    
-    var showGoToExplorer: Bool {
-        return false
-    }
-    
-    var explorerUrl: URL? {
-        return nil
-    }
-    
-    var showGoToChat: Bool {
-        return false
-    }
-    
-    var chatroom: Chatroom? {
-        return nil
-    }
-    
-    var currencyCode: String {
-        return "ETH"
-    }
-    
-}
-
-enum TransactionReceiptStatus: String, Decodable {
-    case fail = "0"
-    case pass = "1"
-    case unknown
-}
-
-/*
- {
- "blockNumber":"3455267",
- "timeStamp":"1529241530",
- "hash":"0x9e2092aa9a278ebdd5cc4e37d626533ec1a480397c101add069817c0934cfa76",
- "nonce":"561145",
- "blockHash":"0xf828955a0911da4a2c207f96b8bffabac804eab7888ec88149ab9867db19b7dd",
- "transactionIndex":"16",
- "from":"0x687422eea2cb73b5d3e242ba5456b782919afc85",
- "to":"0x700bc74dd49044446bcb6a25ae5e725d14538825",
- "value":"1000000000000000000",
- "gas":"314150",
- "gasPrice":"5000000000",
- "isError":"0",
- "txreceipt_status":"1",
- "input":"0x",
- "contractAddress":"",
- "cumulativeGasUsed":"381927",
- "gasUsed":"21000",
- "confirmations":"32316"
- }
- 
- */
