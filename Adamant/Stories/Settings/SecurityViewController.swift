000f653d57aedd2ce24fd2fdb46faead3a21b71e
Pavel Anokhov <p.anokhov@gmail.com>
2018-07-04 14:42:59 +0300

    Security: Notifications and StayIn

--- Adamant/Stories/Settings/SecurityViewController.swift
+++ Adamant/Stories/Settings/SecurityViewController.swift
@@ -13,6 +13,14 @@ import Haring
 
 class SecurityViewController: FormViewController {
 	
+	enum PinpadRequest {
+		case createPin
+		case reenterPin(pin: String)
+		case turnOffPin
+		case turnOnBiometry
+		case turnOffBiometry
+	}
+	
 	// MARK: - Section & Rows
 	
 	enum Sections {
@@ -73,20 +81,32 @@ class SecurityViewController: FormViewController {
 	var router: Router!
 	
 	
+	// MARK: - Properties
+	var showLoggedInOptions = false
+	var pinpadRequest: SettingsViewController.PinpadRequest?
+	
+	
 	// MARK: - Lifecycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.navigationItem.title = String.adamantLocalized.settings.title
 		navigationOptions = .Disabled
+		showLoggedInOptions = accountService.hasStayInAccount
+		
 		
 		// MARK: StayIn
-		
 		// Stay logged in
 		let stayInRow = SwitchRow() {
 			$0.tag = Rows.stayIn.tag
 			$0.title = Rows.stayIn.localized
 			$0.value = accountService.hasStayInAccount
+		}.onChange { [weak self] row in
+			guard let enabled = row.value else {
+				return
+			}
+			
+			self?.setStayLoggedIn(enabled: enabled)
 		}
 		
 		// Biometry
@@ -94,6 +114,17 @@ class SecurityViewController: FormViewController {
 			$0.tag = Rows.biometry.tag
 			$0.title = localAuth.biometryType.localized
 			$0.value = accountService.useBiometry
+			
+			$0.hidden = Condition.function([], { [weak self] _ -> Bool in
+				guard let showBiometry = self?.showLoggedInOptions else {
+					return true
+				}
+				
+				return !showBiometry
+			})
+		}.onChange { [weak self] row in
+			let value = row.value ?? false
+			self?.setBiometry(enabled: value)
 		}
 		
 		let stayInSection = Section(Sections.stayIn.localized) {
@@ -105,7 +136,6 @@ class SecurityViewController: FormViewController {
 		
 		
 		// MARK: Notifications
-		
 		// Type
 		let nType = ActionSheetRow<NotificationsMode>() {
 			$0.tag = Rows.notificationsMode.tag
@@ -115,10 +145,22 @@ class SecurityViewController: FormViewController {
 			$0.value = notificationsService.notificationsMode
 		}.cellUpdate { (cell, _) in
 			cell.accessoryType = .disclosureIndicator
+		}.onChange { [weak self] row in
+			let mode = row.value ?? NotificationsMode.disabled
+			self?.setNotificationMode(mode)
 		}
 		
+		// Section
 		let notificationsSection = Section(Sections.notifications.localized) {
 			$0.tag = Sections.notifications.tag
+			
+			$0.hidden = Condition.function([], { [weak self] _ -> Bool in
+				guard let showNotifications = self?.showLoggedInOptions else {
+					return true
+				}
+				
+				return !showNotifications
+			})
 		}
 		
 		notificationsSection.append(nType)
@@ -126,7 +168,7 @@ class SecurityViewController: FormViewController {
 		
 		
 		// MARK: ANS Description
-		
+		// Description
 		let descriptionRow = TextAreaRow() {
 			$0.textAreaHeight = .dynamic(initialTextViewHeight: 44)
 			$0.tag = Rows.description.tag
@@ -137,6 +179,7 @@ class SecurityViewController: FormViewController {
 			cell.textView.isEditable = false
 		}
 		
+		// Github readme
 		let githubRow = LabelRow() {
 			$0.tag = Rows.github.tag
 			$0.title = Rows.github.localized
@@ -156,5 +199,54 @@ class SecurityViewController: FormViewController {
 		let ansSection = Section(Sections.ansDescription.localized) { $0.tag = Sections.ansDescription.tag }
 		ansSection.append(contentsOf: [descriptionRow, githubRow])
 		form.append(ansSection)
+		
+		
+		// MARK: Notifications
+		NotificationCenter.default.addObserver(forName: Notification.Name.AdamantAccountService.userLoggedIn, object: nil, queue: OperationQueue.main) { [weak self] _ in
+			self?.reloadForm()
+		}
+		
+		NotificationCenter.default.addObserver(forName: Notification.Name.AdamantAccountService.stayInChanged, object: nil, queue: OperationQueue.main) { [weak self] notification in
+			
+			self?.reloadForm()
+		}
+		
+		NotificationCenter.default.addObserver(forName: Notification.Name.AdamantNotificationService.notificationsModeChanged, object: nil, queue: OperationQueue.main) { [weak self] notification in
+			guard let newMode = notification.userInfo?[AdamantUserInfoKey.NotificationsService.newNotificationsMode] as? NotificationsMode else {
+				return
+			}
+			
+			guard let row: ActionSheetRow<NotificationsMode> = self?.form.rowBy(tag: Rows.notificationsMode.tag) else {
+				return
+			}
+			
+			row.value = newMode
+			row.updateCell()
+		}
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		if let indexPath = tableView.indexPathForSelectedRow {
+			tableView.deselectRow(at: indexPath, animated: animated)
+		}
+	}
+	
+	private func reloadForm() {
+		showLoggedInOptions = accountService.hasStayInAccount
+		tableView.reloadData()
+		
+		if let row: SwitchRow = form.rowBy(tag: Rows.stayIn.tag) {
+			row.value = accountService.hasStayInAccount
+		}
+		
+		if let row: SwitchRow = form.rowBy(tag: Rows.biometry.tag) {
+			row.value = accountService.hasStayInAccount && accountService.useBiometry
+			row.evaluateHidden()
+		}
+		
+		if let section = form.sectionBy(tag: Sections.notifications.tag) {
+			section.evaluateHidden()
+		}
 	}
 }
