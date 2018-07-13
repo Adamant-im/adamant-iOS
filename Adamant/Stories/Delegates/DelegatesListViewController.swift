5b9dab0afdabf16aded4895a688fcaad7e0aac8a
Pavel Anokhov <p.anokhov@gmail.com>
2018-07-13 21:41:23 +0300

    DelegatesListViewController refactored

--- Adamant/Stories/Delegates/DelegatesListViewController.swift
+++ Adamant/Stories/Delegates/DelegatesListViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import M13Checkbox
 
 // MARK: - Localization
 extension String.adamantLocalized {
@@ -23,20 +22,26 @@ extension String.adamantLocalized {
 }
 
 class DelegatesListViewController: UIViewController {
-    
-    let cellIdentifier = "cell"
-
+	
     // MARK: - Dependencies
     
     var apiService: ApiService!
     var accountService: AccountService!
     var dialogService: DialogService!
     var router: Router!
-    
+	
+	
+	// MARK: - Constants
+	
+	let votingCost: Decimal = Decimal(integerLiteral: 50)
+	let activeDelegates = 101
+	let maxVotes = 30
+	private let cellIdentifier = "cell"
+	
     // MARK: - Properties
-    let activeDelegates: Int = 101
-    var delegates: [Delegate] = [Delegate]()
-    var newVotesDelegates: [Delegate] = [Delegate]()
+	
+    private (set) var delegates: [Delegate] = [Delegate]()
+    private var delegatesChanges: [IndexPath] = [IndexPath]()
     
     private lazy var refreshControl: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
@@ -47,7 +52,8 @@ class DelegatesListViewController: UIViewController {
         
         return refreshControl
     }()
-    
+	
+	
     // MARK: - IBOutlets
     @IBOutlet weak var tableView: UITableView!
     
@@ -58,6 +64,9 @@ class DelegatesListViewController: UIViewController {
     
     @IBOutlet weak var voteBtn: UIButton!
 
+	
+	// MARK: - Lifecycle
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         if #available(iOS 11.0, *) {
@@ -66,39 +75,17 @@ class DelegatesListViewController: UIViewController {
         
         navigationItem.title = String.adamantLocalized.delegates.title
         
-        tableView.register(UINib.init(nibName: "DelegateCell", bundle: nil), forCellReuseIdentifier: cellIdentifier)
-        tableView.addSubview(self.refreshControl)
+        tableView.register(UINib.init(nibName: "AdamantDelegateCell", bundle: nil), forCellReuseIdentifier: cellIdentifier)
+		tableView.rowHeight = 50
+		tableView.addSubview(self.refreshControl)
         
-        self.upVotesLabel.text = ""
-        self.downVotesLabel.text = ""
-        self.newVotesLabel.text = ""
-        self.totalVotesLabel.text = ""
+        upVotesLabel.text = ""
+        downVotesLabel.text = ""
+        newVotesLabel.text = ""
+        totalVotesLabel.text = ""
         
-        self.refreshControl.beginRefreshing()
-        handleRefresh(self.refreshControl)
-        
-        NotificationCenter.default.addObserver(forName: Notification.Name.AdamantDelegate.stateChanged, object: nil, queue: OperationQueue.main) { [weak self] notification in
-            guard let address = notification.userInfo?[AdamantUserInfoKey.Delegate.address] as? String, let state = notification.userInfo?[AdamantUserInfoKey.Delegate.newState] as? Bool else {
-                return
-            }
-            
-            guard var delegate = self?.delegates.first(where: { (delegate) -> Bool in
-                delegate.address == address
-            }) else {
-                return
-            }
-            
-            if let index = self?.newVotesDelegates.index(where: { (delegate) -> Bool in
-                delegate.address == address
-            }) {
-                self?.newVotesDelegates.remove(at: index)
-            } else {
-                delegate.voted = state
-                self?.newVotesDelegates.append(delegate)
-            }
-            
-            self?.updateVotePanel()
-        }
+        refreshControl.beginRefreshing()
+        handleRefresh(refreshControl)
     }
     
     deinit {
@@ -111,105 +98,38 @@ class DelegatesListViewController: UIViewController {
 		if #available(iOS 11.0, *) {
 			navigationController?.navigationBar.prefersLargeTitles = false
 		}
+		
+		if let indexPath = tableView.indexPathForSelectedRow {
+			tableView.deselectRow(at: indexPath, animated: animated)
+		}
 	}
 
     @objc private func handleRefresh(_ refreshControl: UIRefreshControl) {
-        if let address = accountService.account?.address {
-            apiService.getDelegatesWithVotes(for: address, limit: activeDelegates) { (result) in
-                switch result {
-                case .success(let delegates):
-                    print(delegates)
-                    self.delegates = delegates
-                    DispatchQueue.main.async {
-                        self.tableView.reloadData()
-                    }
-                case .failure(let error):
-                    print(error)
-                    self.dialogService.showError(withMessage: error.localized, error: error)
-                }
-                
-                DispatchQueue.main.async {
-                    refreshControl.endRefreshing()
-                    self.updateVotePanel()
-                }
-            }
-        } else {
-            refreshControl.endRefreshing()
-            self.dialogService.showRichError(error: AccountServiceError.userNotLogged)
-        }
-    }
-    
-    private func updateVotePanel() {
-        if self.newVotesDelegates.count > 0 {
-            voteBtn.isEnabled = true
-        } else {
-            voteBtn.isEnabled = false
-        }
-        
-        DispatchQueue.global().async {
-            let voted = self.delegates.filter({ (delegate) -> Bool in
-                return delegate.voted
-            })
-            
-            let upVoted = self.newVotesDelegates.filter({ (delegate) -> Bool in
-                return delegate.voted == true
-            })
-            
-            let downVoted = self.newVotesDelegates.filter({ (delegate) -> Bool in
-                return delegate.voted == false
-            })
-            
-            DispatchQueue.main.async {
-                self.upVotesLabel.text = "\(upVoted.count)"
-                self.downVotesLabel.text = "\(downVoted.count)"
-                self.newVotesLabel.text = "\(self.newVotesDelegates.count)/30"
-                self.totalVotesLabel.text = "\(voted.count)/\(self.delegates.count)"
-            }
-        }
-    }
-    
-    @IBAction func vote(_ sender: Any) {
-        guard self.newVotesDelegates.count > 0 else {
-            // TODO: Show error message
-            return
-        }
-        if let account = accountService.account, let keypair = accountService.keypair {
-            let balance = (account.balance as NSDecimalNumber).doubleValue
-            if balance > 50 {
-                let voted = self.newVotesDelegates.sorted(by: { (item1, item2) -> Bool in
-                    var check1: Int = 0
-                    var check2: Int = 0
-                    if item1.voted == true {
-                        check1 = 1
-                    }
-                    if item2.voted == true {
-                        check2 = 1
-                    }
-                    return check1 < check2
-                }).map({ (delegate) -> String in
-                    return delegate.voted ? "+\(delegate.publicKey)" : "-\(delegate.publicKey)"
-                })
-                
-                self.apiService.voteForDelegates(from: account.address, keypair: keypair, votes: voted) { (result) in
-                    switch result {
-                    case .success(_):
-                        self.refreshControl.beginRefreshing()
-                        self.handleRefresh(self.refreshControl)
-                        self.dialogService.showSuccess(withMessage: String.adamantLocalized.delegates.success)
-                        break
-                        
-                    case .failure(let error):
-                        self.dialogService.showRichError(error: TransfersProviderError.serverError(error))
-                    }
-                }
-            } else {
-                self.dialogService.showWarning(withMessage: String.adamantLocalized.delegates.notEnoughtTokensForVote)
-            }
-        } else {
-            self.dialogService.showRichError(error: AccountServiceError.userNotLogged)
-        }
+		guard let address = accountService.account?.address else {
+			refreshControl.endRefreshing()
+			self.dialogService.showRichError(error: AccountServiceError.userNotLogged)
+			return
+		}
+		
+		apiService.getDelegatesWithVotes(for: address, limit: activeDelegates) { (result) in
+			switch result {
+			case .success(let delegates):
+				self.delegates = delegates
+				DispatchQueue.main.async {
+					self.tableView.reloadData()
+				}
+			case .failure(let error):
+				self.dialogService.showError(withMessage: error.localized, error: error)
+			}
+			
+			DispatchQueue.main.async {
+				refreshControl.endRefreshing()
+				self.updateVotePanel()
+			}
+		}
     }
 }
+
 
 // MARK: - UITableView
 extension DelegatesListViewController: UITableViewDataSource, UITableViewDelegate {
@@ -225,43 +145,148 @@ extension DelegatesListViewController: UITableViewDataSource, UITableViewDelegat
         return UIView()
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 50
-    }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+		guard let controller = router.get(scene: AdamantScene.Delegates.delegateDetails) as? DelegateDetailsViewController else {
+			return
+		}
+		
         let delegate = delegates[indexPath.row]
-        guard let controller = router.get(scene: AdamantScene.Delegates.delegateDetails) as? DelegateDetailsViewController else {
-            return
-        }
-
         controller.delegate = delegate
+		
         navigationController?.pushViewController(controller, animated: true)
     }
-    
+	
+	// MARK: Cells
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? DelegateCell else {
-                return UITableViewCell(style: .default, reuseIdentifier: "cell")
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? AdamantDelegateCell else {
+			return UITableViewCell(style: .default, reuseIdentifier: "cell")
         }
         
         let delegate = delegates[indexPath.row]
         
         cell.nameLabel.text = delegate.username
-        cell.rankLabel.text = "\(delegate.rank)"
+        cell.rankLabel.text = String(delegate.rank)
         cell.addressLabel.text = delegate.address
-        cell.statusLabel.text = delegate.rank <= activeDelegates ? "●" : "○"
-        
-        if let delegate = self.newVotesDelegates.first(where: { (vote) -> Bool in
-            return vote.address == delegate.address
-        }) {
-            cell.checkbox.checkState = delegate.voted ? .checked : .unchecked
-        } else {
-            cell.checkbox.checkState = delegate.voted ? .checked : .unchecked
-        }
-        
-        cell.accessoryType = .disclosureIndicator
-        
+        cell.delegateIsActive = delegate.rank <= activeDelegates
+		cell.accessoryType = .disclosureIndicator
+		cell.delegate = self
+		cell.checkmarkColor = UIColor.adamantPrimary
+		
+		cell.isUpvoted = delegate.voted
+		
+		cell.setIsChecked(delegatesChanges.contains(indexPath), animated: false)
+		
         return cell
     }
+}
+
+
+// MARK: - AdamantDelegateCellDelegate
+extension DelegatesListViewController: AdamantDelegateCellDelegate {
+	func delegateCell(_ cell: AdamantDelegateCell, didChangeCheckedStateTo state: Bool) {
+		guard let indexPath = tableView.indexPath(for: cell) else {
+			return
+		}
+		
+		var delegate = delegates[indexPath.row]
+		delegate.voted = state
+		delegatesChanges.append(indexPath)
+		
+		updateVotePanel()
+	}
+}
+
+
+// MARK: - Voting
+extension DelegatesListViewController {
+	@IBAction func vote(_ sender: Any) {
+		// MARK: Prepare
+		guard delegatesChanges.count > 0 else {
+			return
+		}
+		
+		guard let account = accountService.account, let keypair = accountService.keypair else {
+			self.dialogService.showRichError(error: AccountServiceError.userNotLogged)
+			return
+		}
+		
+		guard account.balance > votingCost else {
+			self.dialogService.showWarning(withMessage: String.adamantLocalized.delegates.notEnoughtTokensForVote)
+			return
+		}
+		
+		
+		// MARK: Build
+		
+		var votes = [DelegateVote]()
+		
+		for indexPath in delegatesChanges {
+			let delegate = delegates[indexPath.row]
+			let vote: DelegateVote = delegate.voted ? .downvote(publicKey: delegate.publicKey) : .upvote(publicKey: delegate.publicKey)
+			votes.append(vote)
+		}
+		
+		
+		// MARK: Send
+		
+		dialogService.showProgress(withMessage: nil, userInteractionEnable: false)
+		
+		self.apiService.voteForDelegates(from: account.address, keypair: keypair, votes: votes) { [weak self] result in
+			switch result {
+			case .success:
+				self?.dialogService.showSuccess(withMessage: String.adamantLocalized.delegates.success)
+				
+				if let refreshControl = self?.refreshControl {
+					refreshControl.beginRefreshing()
+					self?.handleRefresh(refreshControl)
+				}
+				
+			case .failure(let error):
+				self?.dialogService.showRichError(error: TransfersProviderError.serverError(error))
+			}
+		}
+	}
+}
+
+
+// MARK: - Private
+extension DelegatesListViewController {
+	private func updateVotePanel() {
+		if delegatesChanges.count > 0 {
+			voteBtn.isEnabled = true
+		} else {
+			voteBtn.isEnabled = false
+		}
+		
+		let changes = delegatesChanges.map { delegates[$0.row] }
+		
+		var upvoted = 0
+		var downvoted = 0
+		for delegate in changes {
+			if delegate.voted {
+				downvoted += 1
+			} else {
+				upvoted += 1
+			}
+		}
+		
+		let totalDelegates = delegates.count
+		let totalChanges = delegatesChanges.count
+		let totalVoted = delegates.reduce(0) { $0 + ($1.voted ? 1 : 0) }
+		
+		if Thread.isMainThread {
+			upVotesLabel.text = String(upvoted)
+			downVotesLabel.text = String(downvoted)
+			newVotesLabel.text = "\(totalChanges)/\(maxVotes)"
+			totalVotesLabel.text = "\(totalVoted)/\(totalDelegates)"
+		} else {
+			let max = maxVotes
+			DispatchQueue.main.async {
+				self.upVotesLabel.text = "\(upvoted)"
+				self.downVotesLabel.text = "\(downvoted)"
+				self.newVotesLabel.text = "\(totalChanges)/\(max)"
+				self.totalVotesLabel.text = "\(totalVoted)/\(totalDelegates)"
+			}
+		}
+	}
 }
