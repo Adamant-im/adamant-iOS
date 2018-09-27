ebac01729da78b259b4f12399f0747bfcdbbd3fb
Anton B <anton@absoftware.mobi>
2018-09-27 19:03:42 +0300

    Notification fix for iOS 12

--- Adamant/Services/AdamantNotificationService.swift
+++ Adamant/Services/AdamantNotificationService.swift
@@ -124,6 +124,9 @@ extension AdamantNotificationsService {
 				UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { (granted, error) in
 					completion(granted, error)
 				})
+                
+            case .provisional:
+                break
 			}
 		}
 	}
