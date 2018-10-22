c33b7b3245a960671488cb2bda63d3f5aa5e5f82
Anton B <anton@absoftware.mobi>
2018-10-22 16:11:07 +0300

    Refactor adm address extraction from string or URI

--- Adamant/Helpers/String+adamant.swift
+++ Adamant/Helpers/String+adamant.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+struct AdamantAddress {
+    let address: String
+    let name: String?
+}
+
+extension String {
+    func getAdamantAddress() -> AdamantAddress? {
+        let address: String?
+        var name: String? = nil
+        
+        if let uri = AdamantUriTools.decode(uri: self) {
+            switch uri {
+            case .address(address: let addr, params: let params):
+                address = addr
+                
+                if let params = params {
+                    for param in params {
+                        switch param {
+                        case .label(let label):
+                            name = label
+                            break
+                        }
+                    }
+                }
+                
+            case .passphrase(_):
+                address = nil
+            }
+        } else {
+            switch AdamantUtilities.validateAdamantAddress(address: self) {
+            case .valid, .system:
+                address = self
+                
+            case .invalid:
+                address = nil
+            }
+        }
+        
+        if let address = address {
+            return AdamantAddress(address: address, name: name)
+        } else {
+            return nil
+        }
+    }
+}
