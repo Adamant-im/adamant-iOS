994dc6919eb92daaff3891e27f7a6b45662118bc
Pavel Anokhov <p.anokhov@gmail.com>
2018-12-27 17:09:40 +0300

    NewChat fixed: newChatViewController removed from navigation stack after pushing chatViewController

--- Adamant/Stories/Chats/ChatListViewController.swift
+++ Adamant/Stories/Chats/ChatListViewController.swift
@@ -491,28 +491,32 @@ extension ChatListViewController: NewChatViewControllerDelegate {
                 return
             }
             
-            if let split = self?.splitViewController {
-                let chat = UINavigationController(rootViewController:vc)
-                split.showDetailViewController(chat, sender: self)
-            } else {
-                self?.navigationController?.pushViewController(vc, animated: false)
-            }
-
-            let nvc: UIViewController
+            let navigator: UINavigationController
             if let nav = controller.navigationController {
-                nvc = nav
+                navigator = nav
+            } else if let nav = self?.navigationController {
+                navigator = nav
             } else {
-                nvc = controller
-            }
-
-            nvc.dismiss(animated: true) {
-                vc.becomeFirstResponder()
-
-                if let count = vc.chatroom?.transactions?.count, count == 0 {
-                    vc.messageInputBar.inputTextView.becomeFirstResponder()
+                self?.present(vc, animated: true) {
+                    vc.becomeFirstResponder()
+                    
+                    if let count = vc.chatroom?.transactions?.count, count == 0 {
+                        vc.messageInputBar.inputTextView.becomeFirstResponder()
+                    }
                 }
+                
+                return
             }
             
+            navigator.pushViewController(vc, animated: true)
+            
+            if let index = navigator.viewControllers.firstIndex(of: controller) {
+                navigator.viewControllers.remove(at: index)
+            }
+            
+            if let count = vc.chatroom?.transactions?.count, count == 0 {
+                vc.messageInputBar.inputTextView.becomeFirstResponder()
+            }
 		}
 		
 		// Select row after awhile
