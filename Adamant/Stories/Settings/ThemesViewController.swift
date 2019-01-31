7ca2c0d342d2f545ca95c7e4ed79aad061d53188
Pavel Anokhov <p.anokhov@gmail.com>
2019-01-31 16:40:03 +0300

    Themes cell

--- Adamant/Stories/Settings/ThemesViewController.swift
+++ Adamant/Stories/Settings/ThemesViewController.swift
@@ -21,8 +21,15 @@ class ThemesViewController: UITableViewController {
         navigationItem.title = AccountViewController.Rows.theme.localized
         
         themeKeys = ThemesManager.shared.themes.keys.sorted()
+        
+        // hack to make default theme (Light) always first
+        if let index = themeKeys.firstIndex(of: ThemesManager.shared.defaultTheme.id) {
+            let id = themeKeys.remove(at: index)
+            themeKeys.insert(id, at: 0)
+        }
+        
         checkedRow = themeKeys.firstIndex(of: ThemesManager.shared.currentTheme.id)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ThemesViewController.identifier)
+        tableView.register(UINib(nibName: "ThemeTableViewCell", bundle: nil), forCellReuseIdentifier: ThemesViewController.identifier)
         
         // Styles
         tableView.setStyle(.baseTable)
@@ -51,20 +58,28 @@ class ThemesViewController: UITableViewController {
     
     // MARK: - Cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ThemesViewController.identifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: ThemesViewController.identifier, for: indexPath) as! ThemeTableViewCell
         
         let id = themeKeys[indexPath.row]
         
         if let theme = ThemesManager.shared.themes[id] {
-            cell.textLabel?.text = theme.title
+            cell.titleLabel.text = theme.title
         } else {
-            cell.textLabel?.text = id
+            cell.titleLabel.text = id
         }
         
         if let checkedRow = checkedRow, indexPath.row == checkedRow {
             cell.accessoryType = .checkmark
         } else {
             cell.accessoryType = .none
+        }
+        
+        if let theme = ThemesManager.shared.themes[id] {
+            cell.primaryColor = theme.background
+            cell.secondaryColor = theme.primary
+        } else {
+            cell.primaryColor = ThemesManager.shared.defaultTheme.background
+            cell.secondaryColor = ThemesManager.shared.defaultTheme.primary
         }
         
         cell.setStyle(.baseTableViewCell)
