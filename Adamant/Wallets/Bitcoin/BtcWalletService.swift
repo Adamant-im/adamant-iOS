8ea1a8976be5ceb45893b69d90e24402f641e825
Anton B <anton@absoftware.mobi>
2019-02-18 18:30:40 +0300

    Add BTC address validation

--- Adamant/Wallets/Bitcoin/BtcWalletService.swift
+++ Adamant/Wallets/Bitcoin/BtcWalletService.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 Adamant. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import Swinject
 import BitcoinKit
@@ -54,7 +53,7 @@ class BtcWalletService: WalletService {
     let transactionFeeUpdated = Notification.Name("adamant.btcWallet.feeUpdated")
     
     // MARK: - Properties
-    private var btcWallet: BtcWallet? = nil
+    private (set) var btcWallet: BtcWallet? = nil
     
     private (set) var enabled = true
     
@@ -147,7 +146,48 @@ class BtcWalletService: WalletService {
     }
     
     func validate(address: String) -> AddressValidationResult {
-        return .valid
+        return isValid(bitcoinAddress: address) ? .valid : .invalid
+    }
+
+    private func getBase58DecodeAsBytes(address: String, length: Int) -> [UTF8.CodeUnit]? {
+        let b58Chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+        var output: [UTF8.CodeUnit] = Array(repeating: 0, count: length)
+
+        for i in 0..<address.count {
+            let index = address.index(address.startIndex, offsetBy: i)
+            let charAtIndex = address[index]
+
+            guard let charLoc = b58Chars.index(of: charAtIndex) else { continue }
+
+            var p = b58Chars.distance(from: b58Chars.startIndex, to: charLoc)
+            for j in stride(from: length - 1, through: 0, by: -1) {
+                p += 58 * Int(output[j] & 0xFF)
+                output[j] = UTF8.CodeUnit(p % 256)
+
+                p /= 256
+            }
+
+            guard p == 0 else { return nil }
+        }
+
+        return output
+    }
+
+    public func isValid(bitcoinAddress address: String) -> Bool {
+        guard address.count >= 26 && address.count <= 35,
+            address.rangeOfCharacter(from: CharacterSet.alphanumerics.inverted) == nil,
+            let decodedAddress = getBase58DecodeAsBytes(address: address, length: 25),
+            decodedAddress.count >= 4
+            else { return false }
+
+        let decodedAddressNoCheckSum = Array(decodedAddress.prefix(decodedAddress.count - 4))
+        let hashedSum = decodedAddressNoCheckSum.sha256().sha256()
+
+        let checkSum = Array(decodedAddress.suffix(from: decodedAddress.count - 4))
+        let hashedSumHeader = Array(hashedSum.prefix(4))
+
+        return hashedSumHeader == checkSum
     }
     
     func getWalletAddress(byAdamantAddress address: String, completion: @escaping (WalletServiceResult<String>) -> Void) {
