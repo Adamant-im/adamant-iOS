5942c2f0ce6de55fcda83a639b29f8844ad8ee14
Pavel Anokhov <p.anokhov@gmail.com>
2019-05-26 21:01:36 +0300

    NotificationContentExtension

---
+++ NotificationContentExtension/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  NotificationContentExtension
+//
+//  Created by Anokhov Pavel on 26/05/2019.
+//  Copyright © 2019 Adamant. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
