18bbc0d78d3c064902af8ced0299196cd2edf8d8
Anton Boyarkin <boyarkin.anton@gmail.com>
2019-10-06 17:47:29 +0300

    Disable node

--- AdamantShared/AdamantResources.swift
+++ AdamantShared/AdamantResources.swift
@@ -21,7 +21,7 @@ struct AdamantResources {
         Node(scheme: .https, host: "bid.adamant.im", port: nil),
         Node(scheme: .https, host: "unusual.adamant.im", port: nil),
         Node(scheme: .http, host: "185.231.245.26", port: 36666),
-        Node(scheme: .http, host: "80.211.177.181", port: 36666),
+//        Node(scheme: .http, host: "80.211.177.181", port: 36666),
 //        Node(scheme: .http, host: "80.211.177.181", port: nil), // Bugged one
 //        Node(scheme: .http, host: "163.172.132.38", port: 36667) // Testnet
     ]
