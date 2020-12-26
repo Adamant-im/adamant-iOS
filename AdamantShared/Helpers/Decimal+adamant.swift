0ba3e3669f55e4736dac31b90e6d94c37a924706
Anton Boyarkin <boyarkin.anton@gmail.com>
2020-12-26 17:28:58 +0300

    Update decimal to double conversion

--- AdamantShared/Helpers/Decimal+adamant.swift
+++ AdamantShared/Helpers/Decimal+adamant.swift
@@ -18,7 +18,6 @@ extension Decimal {
     }
     
     var doubleValue: Double {
-//        return (self as NSNumber).doubleValue
-        return Double(self.description) ?? 0 // Fix issue with floation point number, that adds 0.00000000000002 value
+        return NSDecimalNumber(decimal: self).doubleValue
     }
 }
