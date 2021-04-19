0faccee1807132b42ef7a245f6ee9548d904c922
Anton Boyarkin <boyarkin.anton@gmail.com>
2021-04-19 19:04:38 +0300

    Update project config

--- BitcoinKit/Package.swift
+++ BitcoinKit/Package.swift
@@ -1,27 +1,27 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "BitcoinKit",
-//    platforms: [
-//        .iOS(.v10)
-//    ],
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         .library(name: "BitcoinKit", targets: ["BitcoinKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.180")),
-        .package(url: "https://github.com/skywinder/web3swift.git", .upToNextMinor(from: "2.2.0")),
-        .package(url: "https://github.com/vapor-community/random.git", .upToNextMinor(from: "1.2.0"))
+        .package(name: "OpenSSL", url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "1.1.180")),
+        .package(name: "Web3swift", url: "https://github.com/skywinder/web3swift.git", .upToNextMinor(from: "2.2.0")),
+        .package(name: "Random", url: "https://github.com/vapor-community/random.git", .upToNextMinor(from: "1.2.0"))
     ],
     targets: [
         .target(
             name: "BitcoinKit",
-            dependencies: ["BitcoinKitPrivate", "web3swift", "Random"]
+            dependencies: ["BitcoinKitPrivate", "Random", .product(name: "web3swift", package: "Web3swift")]
         ),
         .target(
             name: "BitcoinKitPrivate",
-            dependencies: ["OpenSSL", "web3swift"]
+            dependencies: ["OpenSSL", .product(name: "web3swift", package: "Web3swift")]
         )
     ]
 )
