8a561ea46dd0eae9f1ef492506c3c12b194916ba
Anton Boyarkin <boyarkin.anton@gmail.com>
2021-05-10 18:57:27 +0300

    Fix dash explorer url

--- AdamantShared/AdamantResources.swift
+++ AdamantShared/AdamantResources.swift
@@ -91,7 +91,7 @@ struct AdamantResources {
     //    static let liskExplorerAddress = "https://testnet-explorer.lisk.io/tx/" // LISK Testnet
     
     static let dogeExplorerAddress = "https://dogechain.info/tx/"
-    static let dashExplorerAddress = "https://dashblockexplorer.com/"
+    static let dashExplorerAddress = "https://dashblockexplorer.com/tx/"
     
     private init() {}
 }
