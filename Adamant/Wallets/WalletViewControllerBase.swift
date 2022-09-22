901d8dd1e34b10e878095b7098925de19b48f4ce
StanislavDevIOS <wtfisriddle@gmail.com>
2022-09-22 10:12:33 +0300

    Redesigned ERC20 wallet labels

--- Adamant/Wallets/WalletViewControllerBase.swift
+++ Adamant/Wallets/WalletViewControllerBase.swift
@@ -150,7 +150,13 @@ class WalletViewControllerBase: FormViewController, WalletViewController {
             
             let sendRow = LabelRow() {
                 $0.tag = BaseRows.send.tag
-                $0.title = label
+                if #available(iOS 14.0, *) {
+                    var content = $0.cell.defaultContentConfiguration()
+                    content.attributedText = label
+                    $0.cell.contentConfiguration = content
+                } else {
+                    $0.cell.textLabel?.attributedText = label
+                }
                 $0.cell.selectionStyle = .gray
                 $0.cell.backgroundColor = UIColor.adamant.cellColor
             }.cellUpdate { (cell, _) in
@@ -273,8 +279,8 @@ class WalletViewControllerBase: FormViewController, WalletViewController {
     
     // MARK: - To override
     
-    func sendRowLocalizedLabel() -> String {
-        return BaseRows.send.localized
+    func sendRowLocalizedLabel() -> NSAttributedString {
+        return NSAttributedString(string: BaseRows.send.localized)
     }
     
     func encodeForQr(address: String) -> String? {
