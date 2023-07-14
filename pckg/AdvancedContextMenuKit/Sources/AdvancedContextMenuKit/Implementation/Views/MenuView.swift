9e6f85cc93dcf3eb30197f78ac3f62db60fece1b
StanislavDevIOS <wtfisriddle@gmail.com>
2023-07-14 12:15:02 +0300

    [trello.com/c/x28cXVkB] feat: improved menu design for MacOS

--- pckg/AdvancedContextMenuKit/Sources/AdvancedContextMenuKit/Implementation/Views/MenuView.swift
+++ pckg/AdvancedContextMenuKit/Sources/AdvancedContextMenuKit/Implementation/Views/MenuView.swift
@@ -13,30 +13,14 @@ struct MenuView: View {
     var body: some View {
         VStack(spacing: 10) {
             ForEach(menu.children, id: \.title) { item in
-                Button {
-                    print("action")
-                } label: {
-                    HStack {
-                        Text(item.title)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.horizontal, 10)
-                            .tint(.black)
-                            .foregroundColor(.black)
-                        if let image = item.image {
-                            Image(uiImage: image)
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                                .padding(.horizontal, 10)
-                                .tint(.black)
-                        }
-                    }
-                }
-
+                MenuButtonView(item: item)
+                
                 if menu.children.last != item {
                     Divider()
                 }
             }
         }
-        .padding(10)
+        .padding([.top, .bottom], 10)
         .background(Blur(style: .light, sensetivity: 1.0))
         .cornerRadius(10)
         .shadow(radius: 5)
@@ -44,9 +28,66 @@ struct MenuView: View {
     }
 }
 
-struct PrimaryButtonStyle: ButtonStyle {
-    func makeBody(configuration: Self.Configuration) -> some View {
+struct MenuButtonView: View {
+    @State private var isHovered = false
+    @GestureState private var isLongPressed = false
+    
+    var item: UIMenuElement
+    
+    var body: some View {
+        Button {
+            print("\(item.title) tapped")
+        } label: {
+            HStack {
+                Text(item.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+                    .tint(.black)
+                    .foregroundColor(.black)
+                    .background(
+                        RoundedRectangle(cornerRadius: 8)
+                            .fill(isHovered ? Color.red.opacity(0.8) : Color.blue)
+                    )
+                if let image = item.image {
+                    Image(uiImage: image)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.horizontal, 20)
+                        .tint(.black)
+                }
+            }
+        }
+        .buttonStyle(MenuButtonStyle(isHovered: $isHovered))
+        .contentShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+        .hoverEffect(.lift)
+        .gesture(
+            LongPressGesture(minimumDuration: 0)
+                .updating($isLongPressed) { value, state, _ in
+                    state = value
+                    isHovered = value
+                }
+        )
+        .onHover { hovering in
+            isHovered = hovering
+        }
+        .frame(height: 20)
+    }
+}
+
+struct MenuButtonStyle: ButtonStyle {
+    @Binding var isHovered: Bool
+    
+    func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .background(configuration.isPressed ? Color.red : Color.clear)
+            .foregroundColor(.white)
+            .background(
+                Rectangle()
+                    .fill(
+                        configuration.isPressed
+                        ? Color.gray.opacity(0.15)
+                        : isHovered ? Color.gray.opacity(0.15) : Color.clear
+                    )
+                    .frame(height: 40)
+            )
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
     }
 }
