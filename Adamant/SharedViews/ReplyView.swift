fe29ddad57de3c949b749ebf6858b9cffeb3d8e2
StanislavDevIOS <wtfisriddle@gmail.com>
2023-08-07 14:42:06 +0300

    [trello.com/c/xPO19yjv] fix: text truncation by tail

--- Adamant/SharedViews/ReplyView.swift
+++ Adamant/SharedViews/ReplyView.swift
@@ -104,7 +104,23 @@ final class ReplyView: UIView {
 extension ReplyView {
     func update(with model: MessageModel) {
         backgroundColor = .clear
-        messageLabel.attributedText = model.makeReplyContent().resolveLinkColor()
+        let text = model.makeReplyContent().resolveLinkColor()
+        text.mutableString.replaceOccurrences(
+            of: "\n",
+            with: " ",
+            range: .init(location: .zero, length: text.length)
+        )
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = .byTruncatingTail
+        
+        text.addAttribute(
+            .paragraphStyle,
+            value: paragraphStyle,
+            range: .init(location: .zero, length: text.length)
+        )
+
+        messageLabel.attributedText = text
     }
 }
 
