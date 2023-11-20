7761d31b086dead7126e9c5473059be9cea54959
“just-software-dev” <idontgiveafuck@hotmail.com>
2023-11-20 09:55:52 +0600

    [trello.com/c/YD9Iu6wp] Popup fix

--- PopupKit/Sources/PopupKit/Implementation/Models/PopupCoordinatorModel.swift
+++ PopupKit/Sources/PopupKit/Implementation/Models/PopupCoordinatorModel.swift
@@ -8,60 +8,8 @@
 import UIKit
 
 final class PopupCoordinatorModel: ObservableObject {
-    @Published var top: NotificationModel?
-    @Published var middle: Alert?
-    @Published var bottom: String?
-    
-    var notification: NotificationModel? {
-        get { top }
-        set { top = newValue }
-    }
-    
-    var toastMessage: String? {
-        get { bottom }
-        set { bottom = newValue }
-    }
-    
-    var alert: AlertModel? {
-        get {
-            switch middle {
-            case let .common(model):
-                return model
-            case .none, .advanced:
-                return nil
-            }
-        } set {
-            switch middle {
-            case .none, .common:
-                middle = newValue.map { .common($0) }
-            case .advanced:
-                break
-            }
-        }
-    }
-    
-    var advancedAlert: AdvancedAlertModel? {
-        get {
-            switch middle {
-            case let .advanced(model):
-                return model
-            case .none, .common:
-                return nil
-            }
-        } set {
-            switch middle {
-            case .none, .advanced:
-                middle = newValue.map { .advanced($0) }
-            case .common:
-                break
-            }
-        }
-    }
-}
-
-extension PopupCoordinatorModel {
-    enum Alert {
-        case common(AlertModel)
-        case advanced(AdvancedAlertModel)
-    }
+    @Published var notification: NotificationModel?
+    @Published var alert: AlertModel?
+    @Published var advancedAlert: AdvancedAlertModel?
+    @Published var toastMessage: String?
 }
