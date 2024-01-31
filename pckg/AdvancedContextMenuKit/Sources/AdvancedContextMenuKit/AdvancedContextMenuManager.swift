e7a2d41cc940f655e2fe65d698cab9f8e776c25b
StanislavDevIOS <wtfisriddle@gmail.com>
2024-01-31 16:06:44 +0200

    [trello.com/c/6eEjuLon] fix: add to-do

--- pckg/AdvancedContextMenuKit/Sources/AdvancedContextMenuKit/AdvancedContextMenuManager.swift
+++ pckg/AdvancedContextMenuKit/Sources/AdvancedContextMenuKit/AdvancedContextMenuManager.swift
@@ -207,6 +207,7 @@ extension AdvancedContextMenuManager: OverlayViewDelegate {
         getPositionOnScreen = nil
         // Postpone window dismissal to the next iteration to allow the contentView to become visible
         Task {
+            // TODO: Bug - Occasionally, the copied content view disappears faster than the original view is presented. Fix it later.
             await Task.sleep(interval: 0.1)
             window.rootViewController = nil
             window.isHidden = true
