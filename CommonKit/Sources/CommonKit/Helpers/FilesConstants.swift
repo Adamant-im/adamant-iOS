277fd3448ebcd657c77c05c343cf6efe99a91a45
StanislavDevIOS <wtfisriddle@gmail.com>
2024-04-15 13:50:32 +0300

    [trello.com/c/uxBZaznD] feat: inc max file size to 250 mb

--- CommonKit/Sources/CommonKit/Helpers/FilesConstants.swift
+++ CommonKit/Sources/CommonKit/Helpers/FilesConstants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public final class FilesConstants {
     public static let maxFilesCount = 5
-    public static let maxFileSize: Int64 = 10 * 1024 * 1024
+    public static let maxFileSize: Int64 = 250 * 1024 * 1024
     public static let previewSize: CGSize = .init(squareSize: 400)
     public static let previewTag: String = "preview_"
 }
