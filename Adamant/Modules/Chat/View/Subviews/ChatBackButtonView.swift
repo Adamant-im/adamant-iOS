47cbabe1faa722d6e03b03c18bbaa7581e1dca87
Arkadii Seniiants <arkadii.seniiants@globus.software>
2024-09-02 08:46:34 +0300

    add empty ChatBackButtonView

---
+++ Adamant/Modules/Chat/View/Subviews/ChatBackButtonView.swift
@@ -0,0 +1,22 @@
+//
+//  ChatBackButtonView.swift
+//  Adamant
+//
+//  Created by Аркадий Торвальдс on 30.08.2024.
+//  Copyright © 2024 Adamant. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ChatBackButtonView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
