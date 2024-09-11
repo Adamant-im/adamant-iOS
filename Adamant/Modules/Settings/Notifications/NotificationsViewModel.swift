b642576bca98724bb31960f3601de5d04a21bcd0
Iana <ianasilosievaa@gmail.com>
2024-09-11 23:18:37 +0300

    [trello.com/c/44KDaoe5] Removed unused code

--- Adamant/Modules/Settings/Notifications/NotificationsViewModel.swift
+++ Adamant/Modules/Settings/Notifications/NotificationsViewModel.swift
@@ -51,7 +51,6 @@ final class NotificationsViewModel: ObservableObject {
         Task {
             await addObservers()
             await configure()
-            await setupSwitchHandlers()
         }
     }
     
