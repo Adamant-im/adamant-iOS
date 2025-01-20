44ab1410c9c76a75fad9b475299e84e3eedd69e2
Vladimir Klevtsov <zonda4242@gmail.com>
2025-01-20 19:00:58 +0100

    local setup

---
+++ CommonKit/Sources/CommonKit/AdamantSecret.swift
@@ -0,0 +1,16 @@
+//
+//  AdamantSecret.swift
+//  CommonKit
+//
+//  Created by Владимир Клевцов on 20.1.25..
+//
+import Foundation
+
+enum AdamantSecret {
+    // MARK: App data
+    static let appIdentifierPrefix = "J2L77FMN46"
+    
+    // MARK: Keychain
+    static let oldKeychainPass = "tertherherh"
+    static let keychainValuePassword = "wergwergwerg"
+}
