a7ec9b284e1dcb789d906c39695fb3fc6199a7a5
Ruslan Alikhamov <opensource@evolutions-fz.co>
2025-02-16 22:27:26 +0500

    [trello.com/c/avWMqc8I] wrapped api into viewModel

--- Adamant/Helpers/PinPadView.swift
+++ Adamant/Helpers/PinPadView.swift
@@ -35,14 +35,31 @@ struct PinPadViewRepresentable: UIViewRepresentable {
 }
 
 class PinPadViewModel: ObservableObject {
-    @Published var enteredPin: String = ""
+    enum Mode {
+        case enterPin
+        case createPin
+        case wrongPinEntered
+        case pinNotMatching
+        case reenterPin
+        case pinCreated
+        case pinValidated
+        case turnOffPin
+        case turnOnBiometry
+        case turnOffBiometry
+    }
+    @Published private(set) var mode: Mode
+    @Published private var enteredPin: String = ""
+    @Published private var previousEntry: String = ""
+    @Published private(set) var title: String = ""
+    @Published private(set) var titleColor: Color = .white
     let pinLength: Int
-    let validatePin: (String) -> Bool
+    private let validatePin: (String) -> Bool
     let onSuccess: () -> Void
     let onCancel: () -> Void
     
     init(
         pinLength: Int,
+        mode: Mode,
         validatePin: @escaping (String) -> Bool,
         onSuccess: @escaping () -> Void,
         onCancel: @escaping () -> Void
@@ -51,31 +68,121 @@ class PinPadViewModel: ObservableObject {
         self.validatePin = validatePin
         self.onSuccess = onSuccess
         self.onCancel = onCancel
+        self.mode = mode
+        update(mode: mode)
     }
     
-    func append(_ text: String) {
-        enteredPin.append(text)
+    func append(_ character: String) {
+        enteredPin.append(character)
+    }
+    
+    func removeLast() {
+        enteredPin.removeLast()
+    }
+    
+    func update(mode: Mode) {
+        let title: String
+        let titleColor: Color
+        switch mode {
+        case .createPin:
+            title = "Create new PIN"
+            titleColor = .white
+        case .enterPin:
+            title = "Login into ADAMANT"
+            titleColor = .white
+        case .reenterPin:
+            title = "Re-enter new PIN"
+            titleColor = .white
+        case .wrongPinEntered:
+            title = "Wrong PIN entered!"
+            titleColor = .red
+        case .pinNotMatching:
+            title = "PIN doesn't match!"
+            titleColor = .red
+        case .pinValidated:
+            title = "Success!"
+            titleColor = .green
+        case .pinCreated:
+            title = "PIN created!"
+            titleColor = .green
+        case .turnOffBiometry, .turnOffPin, .turnOnBiometry:
+            title = ""
+            titleColor = .white
+        }
+        self.title = title
+        self.titleColor = titleColor
+        self.mode = mode
+    }
+    
+    func updateCache(mode: Mode) {
+        switch mode {
+        case .createPin:
+            enteredPin.removeAll()
+            previousEntry.removeAll()
+        case .enterPin:
+            return
+        case .reenterPin:
+            previousEntry = enteredPin
+            enteredPin.removeAll()
+        case .wrongPinEntered:
+            enteredPin.removeAll()
+        case .pinNotMatching:
+            enteredPin.removeAll()
+            previousEntry.removeAll()
+        case .pinValidated:
+            return
+        case .pinCreated:
+            return
+        case .turnOffBiometry, .turnOffPin, .turnOnBiometry:
+            return
+        }
+        self.title = title
+        self.titleColor = titleColor
+    }
+    
+    func isValid(mode: Mode) -> Bool {
+        switch mode {
+        case .reenterPin:
+            previousEntry == enteredPin
+        case .wrongPinEntered,
+                .createPin,
+                .enterPin,
+                .pinNotMatching,
+                .pinValidated,
+                .pinCreated,
+                .turnOffBiometry,
+                .turnOffPin,
+                .turnOnBiometry:
+            true
+        }
+    }
+    
+    var isEmpty: Bool {
+        enteredPin.isEmpty
+    }
+    
+    var isPinEnteredCompletely: Bool {
+        enteredPin.count == pinLength
+    }
+    
+    func hasNumber(at index: Int) -> Bool {
+        index < enteredPin.count
+    }
+    
+    var isPinValid: Bool {
+        validatePin(enteredPin)
     }
 }
 
 // swiftlint:disable multiple_closures_with_trailing_closure
 struct PinPadView: View {
-    
-    enum PinPadViewMode {
-        case createPin
-        case reenterPin(pin: String)
-        case turnOffPin
-        case turnOnBiometry
-        case turnOffBiometry
-    }
-    
     @StateObject var viewModel: PinPadViewModel
     
     var body: some View {
         VStack {
             Spacer()
-            Text("Login into ADAMANT")
-                .foregroundColor(.white)
+            Text(viewModel.title)
+                .foregroundColor(viewModel.titleColor)
                 .textCase(nil)
                 .font(.body)
                 .padding(.top, 30)
@@ -84,7 +191,7 @@ struct PinPadView: View {
                 ForEach(0..<viewModel.pinLength, id: \.self) { index in
                     Circle()
                         .frame(width: 15, height: 15)
-                        .foregroundColor(index < viewModel.enteredPin.count ? .white : .gray)
+                        .foregroundColor(viewModel.hasNumber(at: index) ? .white : .gray)
                 }
             }
             .padding(.vertical, 20)
@@ -146,20 +253,57 @@ struct PinPadView: View {
     }
     
     private func handlePinInput(_ digit: String) {
-        guard viewModel.enteredPin.count < viewModel.pinLength else { return }
-        viewModel.enteredPin.append(digit)
-        if viewModel.enteredPin.count == viewModel.pinLength {
-            if viewModel.validatePin(viewModel.enteredPin) {
-                viewModel.onSuccess()
-            } else {
-                viewModel.enteredPin.removeAll()
+        viewModel.append(digit)
+        if viewModel.isPinEnteredCompletely {
+            switch viewModel.mode {
+            case .enterPin:
+                if viewModel.isPinValid {
+                    viewModel.update(mode: .pinValidated)
+                    viewModel.onSuccess()
+                } else {
+                    viewModel.update(mode: .wrongPinEntered)
+                    Task.detached { @MainActor in
+                        viewModel.update(mode: .enterPin)
+                        viewModel.updateCache(mode: .enterPin)
+                    }
+                }
+            case .createPin:
+                viewModel.update(mode: .reenterPin)
+                Task.detached { @MainActor in
+                    try await Task.sleep(nanoseconds: 300_000_000)
+                    viewModel.updateCache(mode: .reenterPin)
+                }
+            case .reenterPin:
+                if viewModel.isValid(mode: .reenterPin) {
+                    viewModel.update(mode: .pinCreated)
+                    Task.detached { @MainActor in
+                        try await Task.sleep(nanoseconds: 100_000_000)
+                        viewModel.updateCache(mode: .pinCreated)
+                        viewModel.onSuccess()
+                    }
+                } else {
+                    viewModel.update(mode: .pinNotMatching)
+                    Task.detached { @MainActor in
+                        try await Task.sleep(nanoseconds: 500_000_000)
+                        viewModel.update(mode: .createPin)
+                        viewModel.updateCache(mode: .createPin)
+                    }
+                }
+            case .turnOffBiometry,
+                    .turnOffPin,
+                    .turnOnBiometry,
+                    .pinCreated,
+                    .wrongPinEntered,
+                    .pinNotMatching,
+                    .pinValidated:
+                return
             }
         }
     }
-
+    
     private func deleteLastDigit() {
-        guard !viewModel.enteredPin.isEmpty else { return }
-        viewModel.enteredPin.removeLast()
+        guard !viewModel.isEmpty else { return }
+        viewModel.removeLast()
     }
 }
 
@@ -169,6 +313,7 @@ struct PinPadView: View {
         viewModel:
             PinPadViewModel(
                 pinLength: 6,
+                mode: .createPin,
                 validatePin:
                     { _ in
                         true
