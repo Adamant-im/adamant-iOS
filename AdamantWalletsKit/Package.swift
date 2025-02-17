9be130ab58493941a19146fcbd9f7f26bbee03d4
Vladimir Klevtsov <zonda4242@gmail.com>
2025-02-17 15:57:42 +0100

    [trello.com/c/txGIRwKv] update dependancy branch to dev

--- AdamantWalletsKit/Package.swift
+++ AdamantWalletsKit/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/Adamant-im/adamant-wallets.git",
-            branch: "trello.com/c/txGIRwKv"
+            branch: "dev"
         )
     ],
     targets: [
