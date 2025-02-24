1c1d123dd714caa449280d7dea6adc394261ee75
Christian <kpbenua@yandex.ru>
2025-02-24 22:01:59 +0300

    [trello.com/c/G77vaoVI]: login right after pasting using paste button (#709)
    * [trello.com/c/G77vaoVI]: login right after pasting using paste button
    
    * [trello.com/c/G77vaoVI]: allow editing after pasting wrong passphrase
    
    * [trello.com/c/G77vaoVI]: added auto-login after pasting using cmd+v or native ios textfield popup

--- Adamant/Modules/Login/LoginViewController.swift
+++ Adamant/Modules/Login/LoginViewController.swift
@@ -226,11 +226,22 @@ final class LoginViewController: FormViewController {
         }
         
         // Passphrase row
-        <<< PasswordRow {
+        <<< PasteInterceptingPasswordRow {
             $0.tag = Rows.passphrase.tag
             $0.placeholder = Rows.passphrase.localized
             $0.placeholderColor = UIColor.adamant.secondary
-            $0.cell.textField.enablePasteButtonAndPasswordToggle()
+            $0.cell._textField.pasteInterceptor = { [weak self] text in
+                if let text {
+                    self?.loginWith(passphrase: text)
+                }
+            }
+            $0.cell.textField.enablePasteButtonAndPasswordToggle { [weak self] in
+                // assing text to textfield like this to make loginButton update its enabled/disabled state
+                let row = self?.form.rowBy(tag: Rows.passphrase.tag) as? PasteInterceptingPasswordRow
+                row?.value = $0
+                row?.cell.textField.text = $0
+                self?.loginWith(passphrase: $0)
+            }
             $0.keyboardReturnType = KeyboardReturnTypeConfiguration(nextKeyboardType: .go, defaultKeyboardType: .go)
             }
             
@@ -550,9 +561,9 @@ extension LoginViewController {
 // MARK: UITextField + extensions
 
 private extension UITextField {
-    func enablePasteButtonAndPasswordToggle() {
+    func enablePasteButtonAndPasswordToggle(_ pasteButtonHandler: @escaping (String) -> Void) {
         let passwordToggleButton = makePasswordButton()
-        let pasteButton = makePasteButton()
+        let pasteButton = makePasteButton(pasteButtonHandler)
         
         let containerView = UIView()
         let buttonStack = UIStackView(arrangedSubviews: [pasteButton, passwordToggleButton])
@@ -579,17 +590,23 @@ private extension UITextField {
         rightViewMode = .always
     }
     
-    func makePasteButton() -> UIButton {
+    func makePasteButton(_ handler: @escaping (String) -> Void) -> UIButton {
         let button = UIButton(type: .custom)
         button.imageEdgeInsets = UITextField.buttonImageEdgeInsets
         button.setImage(.asset(named: "clipboard"), for: .normal)
-        button.addTarget(self, action: #selector(pasteFromPasteboard(_:)), for: .touchUpInside)
+        button.addAction(
+            UIAction { [weak self] _ in
+                self?.pasteFromPasteboard(handler)
+            },
+            for: .touchUpInside
+        )
         return button
     }
     
-    @objc func pasteFromPasteboard(_ sender: UIButton) {
+    @objc func pasteFromPasteboard(_ handler: @escaping (String) -> Void) {
         if let pasteboardText = UIPasteboard.general.string {
-            self.text = pasteboardText
+            let newText = String(pasteboardText.prefix(150))
+            handler(newText)
         }
     }
 }
