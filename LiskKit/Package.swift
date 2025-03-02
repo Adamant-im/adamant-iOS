64b106166a90de2006c732753a370fdf1861fe0c
ChrisBenua <kpbenua@edu.hse.ru>
2025-03-02 22:45:52 +0300

    [trello.com/c/Y5q62UTf]: migrate personal fork to adamant account

--- LiskKit/Package.swift
+++ LiskKit/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "Sodium", url: "https://github.com/jedisct1/swift-sodium.git", from: "0.9.1"),
-        .package(url: "https://github.com/ChrisBenua/CryptoSwift.git", from: "1.3.0"),
+        .package(url: "https://github.com/Adamant-im/CryptoSwift.git", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0")
     ],
     targets: [
