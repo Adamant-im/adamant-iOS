b359fd8ef4125ad4b490fc3dccf8ac66523c473e
Ruslan Alikhamov <opensource@evolutions-fz.co>
2025-04-01 02:49:47 +0500

    [trello.com/c/oOCxsoA5-3] added mocks command

--- scripts/AdamantCLI/Sources/AdamantCLI/AdamantCLI.swift
+++ scripts/AdamantCLI/Sources/AdamantCLI/AdamantCLI.swift
@@ -1,6 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
-// 
+//
 // Swift Argument Parser
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
@@ -10,10 +10,10 @@ import ArgumentParser
 struct AdamantCLI: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "Adamant-CLI Toolkit.",
-        subcommands: [Hooks.self, Format.self, Setup.self]
+        subcommands: [Hooks.self, Format.self, Setup.self, Mocks.self]
     )
-    
+
     mutating func run() async throws {
-        
+
     }
 }
