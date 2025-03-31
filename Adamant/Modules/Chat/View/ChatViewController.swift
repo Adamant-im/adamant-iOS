c27f72fd146ef9179f7d22366c382630abb206c3
Vladimir Klevtsov <zonda4242@gmail.com>
2025-03-31 11:35:38 +0200

    [trello.com/c/jKqpxiCa] update new message line and chat entering position

--- Adamant/Modules/Chat/View/ChatViewController.swift
+++ Adamant/Modules/Chat/View/ChatViewController.swift
@@ -328,6 +328,11 @@ private extension ChatViewController {
             .removeDuplicates()
             .sink { [weak self] _ in
                 self?.updateMessages()
+            }
+            .store(in: &subscriptions)
+        
+        viewModel.messagesUpdated
+            .sink { [weak self] _ in
                 self?.updateMessagesPosition()
             }
             .store(in: &subscriptions)
@@ -414,12 +419,14 @@ private extension ChatViewController {
             .sink { [weak self] in self?.processFileToolbarView($0) }
             .store(in: &subscriptions)
         
-        viewModel.$scrollToMessage
+        viewModel.$scrollToIdAndPosition
             .sink { [weak self] in
-                guard let toId = $0
+                guard let idAndPosition = $0
                 else { return }
                 
-                self?.scrollToPosition(.messageId(toId), animated: false)
+                self?.scrollToPosition = idAndPosition.position
+                self?.scrollToPosition(.messageId(idAndPosition.id), animated: false, setExtraOffset: self?.scrollToPosition == .top)
+                self?.viewModel.messageIdToShow = nil
             }
             .store(in: &subscriptions)
         
@@ -761,10 +768,12 @@ private extension ChatViewController {
     func updateMessagesPosition() {
         guard !messagesLoaded, !viewModel.messages.isEmpty else { return }
         messagesLoaded = true
-        if let position = viewModel.startPosition {
-            scrollToPosition(position)
-        } else if let unreadMessage = viewModel.unreadMessagesIds?.first {
-            scrollToPosition(.messageId(unreadMessage), animated: false)
+        if viewModel.messageIdToShow == nil {
+            if let unreadMessage = viewModel.unreadMessagesIds?.first {
+                scrollToPosition(.messageId(unreadMessage), setExtraOffset: true)
+            } else if let position = viewModel.startPosition {
+                scrollToPosition(position)
+            }
         }
     }
     
@@ -920,7 +929,7 @@ private extension ChatViewController {
     }
     
     @MainActor
-    func scrollToPosition(_ position: ChatStartPosition, animated: Bool = false) {
+    func scrollToPosition(_ position: ChatStartPosition, animated: Bool = false, setExtraOffset: Bool = false) {
         chatMessagesCollectionView.fixedBottomOffset = nil
         
         switch position {
@@ -944,6 +953,10 @@ private extension ChatViewController {
                 animated: animated
             )
             
+            if setExtraOffset {
+                setExtraOffsetForNewMessages()
+            }
+            
             viewModel.needToAnimateCellIndex = needToAnimateCell
             ? index
             : nil
@@ -958,6 +971,17 @@ private extension ChatViewController {
         
         guard !viewAppeared else { return }
         chatMessagesCollectionView.fixedBottomOffset = chatMessagesCollectionView.bottomOffset
+    }
+    
+    func setExtraOffsetForNewMessages() {
+        //extra scroll to 120 to see newMessage line and 2 strings from previus message
+        let newOffsetY = max(
+            messagesCollectionView.contentOffset.y - 120,
+            0
+        )
+        let newOffset = CGPoint(x: messagesCollectionView.contentOffset.x, y: newOffsetY)
+        
+        messagesCollectionView.setContentOffset(newOffset, animated: false)
     }
     
     func scrollDownOnNewMessageIfNeeded(previousBottomMessageId: String?) {
