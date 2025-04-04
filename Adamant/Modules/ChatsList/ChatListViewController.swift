b0813ec36a1b7929ac6685ed1feb0ed9b3a980e9
Vladimir Klevtsov <zonda4242@gmail.com>
2025-04-04 13:38:04 +0200

    [trello.com/c/TOuHqbBz] add remove adress from keychain when entering the chat

--- Adamant/Modules/ChatsList/ChatListViewController.swift
+++ Adamant/Modules/ChatsList/ChatListViewController.swift
@@ -1434,6 +1434,9 @@ extension ChatListViewController {
     private func removeManualAdress(adress: String?) {
         if let adress {
             chatsManuallyMarkedAsUnread.remove(adress)
+            Task {
+                await chatsProvider.removeManualMarkChatAsUnread(chatroomId: adress)
+            }
         }
     }
 }
