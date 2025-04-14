0b3bb6c58187736cb2692ef1a84eb8cf6c7bef82
Dmitrij Meidus <Meidus@mail.ru>
2025-04-14 16:58:29 +0300

    [trello.com/c/QBfqpo6A] Make minimum width 400 for primary column

--- Adamant/App/AppDelegate.swift
+++ Adamant/App/AppDelegate.swift
@@ -777,7 +777,7 @@ private func makeSplitController() -> UISplitViewController {
     
     let minimumPrimaryColumnWidth: CGFloat = UIScreen.main.bounds.width * 0.2
     // Set the minimum ratio to 1:5, or to 300px if 1:5 results in a smaller value
-    controller.minimumPrimaryColumnWidth = minimumPrimaryColumnWidth > 300 ? minimumPrimaryColumnWidth : 300
+    controller.minimumPrimaryColumnWidth = minimumPrimaryColumnWidth > 400 ? minimumPrimaryColumnWidth : 400
     
     // Set the maximum ratio to 3:1
     controller.maximumPrimaryColumnWidth = UIScreen.main.bounds.width * 0.75
