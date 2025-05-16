a4eb372a47594b1bece3b2c8ab1f3670aa0a4d0a
Vladimir Klevtsov <zonda4242@gmail.com>
2025-05-16 17:42:07 +0200

    [trello.com/c/sG80IZan/v2] fix reading Under transaction details

--- Adamant/Modules/Chat/View/ChatViewController.swift
+++ Adamant/Modules/Chat/View/ChatViewController.swift
@@ -163,16 +163,19 @@ final class ChatViewController: MessagesViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         defer {
-            state.isViewAppeared = true
-            updateUnreadMessages()
+            state.isFirstTimeViewAppeared = true
+            if state.isFirstTimeViewAppeared == true {
+                state.isViewDissappeared.toggle()
+                updateUnreadMessages()
+            }
         }
         inputBar.isUserInteractionEnabled = true
         chatMessagesCollectionView.fixedBottomOffset = nil
-        if !state.isViewAppeared {
+        if !state.isFirstTimeViewAppeared {
             viewModel.presentKeyboardOnStartIfNeeded()
         }
 
-        guard isMacOS, !state.isViewAppeared else { return }
+        guard isMacOS, !state.isFirstTimeViewAppeared else { return }
         focusInputBarWithoutAnimation()
     }
 
@@ -191,6 +194,7 @@ final class ChatViewController: MessagesViewController {
                 ? nil
             : chatMessagesCollectionView.bottomOffset, collectionHeight: messagesCollectionView.contentSize.height
         )
+        state.isViewDissappeared = true
     }
     
     override func collectionView(
@@ -228,7 +232,7 @@ final class ChatViewController: MessagesViewController {
             updateDateHeaderIfNeeded()
         }
         guard
-            state.isViewAppeared,
+            state.isFirstTimeViewAppeared,
             scrollView.contentOffset.y <= viewModel.minOffsetForStartLoadNewMessages
         else { return }
 
@@ -860,7 +864,7 @@ extension ChatViewController {
     }
 
     fileprivate func updateDateHeaderIfNeeded() {
-        guard state.isViewAppeared else { return }
+        guard state.isFirstTimeViewAppeared else { return }
 
         let targetY: CGFloat = targetYOffset + view.safeAreaInsets.top
         let visibleIndexPaths = messagesCollectionView.indexPathsForVisibleItems
@@ -983,9 +987,9 @@ extension ChatViewController {
                 finalOffset += heightDiff
             }
             
-            chatMessagesCollectionView.setBottomOffset(finalOffset, safely: state.isViewAppeared)
-            state.isAutoScrolling = false 
-            guard !state.isViewAppeared else { return }
+            chatMessagesCollectionView.setBottomOffset(finalOffset, safely: state.isFirstTimeViewAppeared)
+            state.isAutoScrolling = false
+            guard !state.isFirstTimeViewAppeared else { return }
             chatMessagesCollectionView.fixedBottomOffset = chatMessagesCollectionView.bottomOffset
         case let .messageId(id, scrollToBottomIfNotFound):
             var index = viewModel.messages.firstIndex(where: { $0.messageId == id })
@@ -1123,7 +1127,7 @@ extension ChatViewController {
                 )
             }
 
-            if state.isViewAppeared {
+            if state.isFirstTimeViewAppeared {
                 messageInputBar.inputTextView.becomeFirstResponder()
             }
         }
@@ -1157,7 +1161,7 @@ extension ChatViewController {
                     )
                 }
             )
-            if state.isViewAppeared {
+            if state.isFirstTimeViewAppeared {
                 messageInputBar.inputTextView.becomeFirstResponder()
             }
         }
