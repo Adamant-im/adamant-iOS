87121c3248cde5b2a195b5c8e9961f89f04cbd50
Dmitrij Meidus <Meidus@mail.ru>
2025-07-31 17:19:10 +0300

    [trello.com/c/ouNP4Ezx] Fix Bitcoin history bug

--- Adamant/Modules/Wallets/Bitcoin/BtcWalletService.swift
+++ Adamant/Modules/Wallets/Bitcoin/BtcWalletService.swift
@@ -770,7 +770,7 @@ extension BtcWalletService {
     func getTransactionsHistory(offset: Int, limit: Int) async throws -> [TransactionDetails] {
         let txId =
             offset == .zero
-            ? transactions.first?.txId
+            ? nil
             : transactions.last?.txId
 
         return try await getTransactions(fromTx: txId)
