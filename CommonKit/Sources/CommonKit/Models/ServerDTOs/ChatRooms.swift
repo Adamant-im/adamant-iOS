9ae4fb4c3f387fd1d209553de6c7309c80a79163
Dmitrij Meidus <Meidus@mail.ru>
2025-09-09 18:53:46 +0300

    [trello.com/c/d05y5j] Hotfix: count field type (Release v3.10.1) improvments

--- CommonKit/Sources/CommonKit/Models/ServerDTOs/ChatRooms.swift
+++ CommonKit/Sources/CommonKit/Models/ServerDTOs/ChatRooms.swift
@@ -22,13 +22,7 @@ public struct ChatRooms : Codable, Sendable {
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         chats = try values.decodeIfPresent([ChatRoomsChats].self, forKey: .chats)
-        if let intVal = try? values.decode(Int.self, forKey: .count) {
-            count = intVal
-        } else if let strVal = try? values.decode(String.self, forKey: .count) {
-            count = Int(strVal)
-        } else {
-            count = 0
-        }
+        count = try values.decodeIntOrString(forKey: .count) ?? 0
         messages = try values.decodeIfPresent([Transaction].self, forKey: .messages)
     }
 }
